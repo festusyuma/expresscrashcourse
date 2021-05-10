97e3cc3ca22503ac5574e8e87f82ca5e01814372
festus agboma <festusyuma@gmail.com>
2021-05-10 20:27:28 +0100

    crashcourse

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import path from 'path'
 import fs from 'fs'
+import expressHandlebars from 'express-handlebars'
 import logger from './middleware/logger.js'
+import members from "./Members.js";
 
 // Import route
 import membersRoute from './routes/api/members.js'
@@ -10,12 +12,22 @@ const __dirname = fs.realpathSync('.')
 const app = express()
 const PORT = process.env.PORT || 5000
 
+// Handlebars middleware
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
 // Init middleware
 // app.use(logger)
 app.use(express.json()) // Handles JSON
 app.use(express.urlencoded({ extended: false })) // Handles form data
 
 // Set routes
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Member App',
+        members,
+    })
+})
 app.use('/api/members', membersRoute)
 
 // Set a static folder
